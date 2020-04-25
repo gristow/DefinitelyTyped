460bc031359439cf9e056b0de2a0eb60af54a8e8
Gregory Ristow <gristow@utheory.com>
2020-04-25 14:47:46 -0400

    fix(bloomfilter): Has no default export

--- types/bloomfilter/index.d.ts
+++ types/bloomfilter/index.d.ts
@@ -3,31 +3,29 @@
 // Definitions by: slawiko <https://github.com/slawiko>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module "bloomfilter" {
-    export = BloomFilter;
-}
+declare module 'bloomfilter' {
+    export class BloomFilter {
+        buckets: Int32Array;
 
-declare class BloomFilter {
-    buckets: Int32Array;
+        /**
+         * Create a new empty bloom filter of size m with hashes k or
+         * provide buckets as a number[] or Int32Array to deserialize a bloom filter
+         * @param mOrBucketsArray number of bits (will be rounded up to nearest 32), or buckets
+         * to deserialize into a filled bloomfilter
+         * @param k number of hashes
+         */
+        constructor(m: number | number[] | Int32Array, k: number);
 
-    /**
-     * Create a new empty bloom filter of size m with hashes k or
-     * provide buckets as a number[] or Int32Array to deserialize a bloom filter
-     * @param mOrBucketsArray number of bits (will be rounded up to nearest 32), or buckets
-     * to deserialize into a filled bloomfilter
-     * @param k number of hashes
-     */
-    constructor(m: number | number[] | Int32Array, k: number);
+        /**
+         * Add a value to a bloom filter
+         * @param value
+         */
+        add(value: any): void;
 
-    /**
-     * Add a value to a bloom filter
-     * @param value
-     */
-    add(value: any): void;
-
-    /**
-     * Test whether a value exists in a bloom filter. (False positives are
-     * possible, false negatives are not.)
-     */
-    test(value: any): boolean;
+        /**
+         * Test whether a value exists in a bloom filter. (False positives are
+         * possible, false negatives are not.)
+         */
+        test(value: any): boolean;
+    }
 }
